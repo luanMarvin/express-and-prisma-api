14234abf8abb3700fb052f11a9172fb56f6adc25
luanMarvin <luanmarvin@gmail.com>
2024-01-04 19:41:06 -0300

    test: setting up testing environment

---
+++ __tests__/vote-route/postVote.test.ts
@@ -0,0 +1,9 @@
+describe('Teste Temporário', () => {
+    test('Pronto para operar e aguardando mudanças no código', () => {
+      const result: Number = 2;
+  
+      expect(result).toBeGreaterThan(1);
+  
+      console.log('Esse é um teste temporário e sofrerá correção assim que o código for implementado.');
+    });
+  });
