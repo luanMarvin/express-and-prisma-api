3880c467e15ad2eddc422f256fa3d33d44e49d9d
unknown <luanmarvin@gmail.com>
2024-01-17 21:39:07 -0300

    database: added 'title' field to post model. chore: implement post creation in postPost function

--- src/controllers/posts/index.ts
+++ src/controllers/posts/index.ts
@@ -1,16 +1,46 @@
 import { Request, Response } from 'express';
 import prisma from '../../database/prisma';
 
+interface CreateNewPost {
+    title: string;
+    content: string;
+    authorId: number;
+}
+
 export async function getPost(req: Request, res: Response) {
     //Obtém dados de uma postagem.
 }
 
-export function getAllPost(req: Request, res: Response){
-    //Obtém informações de todas as postagens.
+export async function getAllPost(req: Request, res: Response){
+    try {
+        const allPosts = await prisma.post.findMany();
+        res.status(200).json(allPosts);
+    } catch (error) {
+        res.status(500).send(error);
+    } finally {
+        await prisma.$disconnect();
+    }
 }
 
-export function postPost(req: Request, res: Response) {
-    //Cria uma nova postagem.
+export async function postPost(req: Request, res: Response) {
+    try {
+        const data: CreateNewPost = req.body;
+        const posts = await prisma.post.create({
+            data: {
+                title: data.title,
+                content: data.content,
+                author: { connect: { id: data.authorId } },
+            },
+          })
+          res.status(201).json({
+            'Titulo': data.title,
+            'Conteudo': data.content
+          })
+    } catch (error) {
+        res.status(500).send(error)
+    } finally {
+        await prisma.$disconnect();
+    }
 }
 
 export function putPost(req: Request, res: Response) {
